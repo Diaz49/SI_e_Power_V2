d0bf4afcb1fb494e0c4d6fb3a604c93782f3e83b
fagiln <fagilnuril18@gmail.com>
2024-11-13 09:26:41 +0700

    [fix] create delete

--- resources/views/master/data-project-id/action.blade.php
+++ resources/views/master/data-project-id/action.blade.php
@@ -3,9 +3,10 @@
         <i class="fas fa-ellipsis-h"></i>
     </button>
     <ul class="dropdown-menu">
-        <li><a class="dropdown-item text-info fw-bold d-flex justify-content-between" type="button">Edit <i
-                    class="ml-4 fas fa-pen"></i></a></li>
-        <li><button class="btn dropdown-item text-danger fw-bold d-flex justify-content-between" type="button"
+        <li><button class="dropdown-item text-info fw-bold d-flex justify-content-between" type="button" >Edit <i
+                    class="ml-4 fas fa-pen"></i></button></li>
+        <li>
+            <button class="dropdown-item text-danger fw-bold d-flex justify-content-between" type="button"
                 data-table-id="dataprojectid-table" data-url="{{ route('project-id.delete', $projectid->id) }}"
                 data-name="{{ $projectid->nama_project }}" data-action="delete">
                 Delete <i class="fas fa-trash"></i>
--- resources/views/master/data-project-id/index.blade.php
+++ resources/views/master/data-project-id/index.blade.php
@@ -6,7 +6,7 @@
     <div class="d-flex justify-content-end">
         <div class="row ">
             <div class="col-12 d-flex justify-content-end mb-3 ">
-                <button class="btn btn-outline-secondary btn-sm me-4" data-bs-target="#modal" data-bs-toggle="modal"><i
+                <button class="btn btn-outline-secondary btn-sm me-4" data-bs-target="#modalTambah" data-bs-toggle="modal"><i
                         class="fas fa-plus"></i> Data Project ID</button>
             </div>
             <div class="col-12 d-flex justify-content-end">
@@ -26,14 +26,14 @@
         </div>
     </div>
 
-    <!-- Modal -->
+    <!-- Modal Tambah -->
     <form action="{{ route('project-id.store') }}" method="POST">
         @csrf
-        <div class="modal fade" id="modal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal fade" id="modalTambah" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog modal-dialog-centered">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">Tambah Data Project ID</h1>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
@@ -89,7 +89,68 @@
         </div>
     </form>
 
+   <!-- Modal Edit -->
+   <form action="{{ route('project-id.store') }}" method="POST">
+    @csrf
+    <div class="modal fade" id="modalEdit" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Edit Data Project ID</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="mb-1 label">Project ID</div>
+                    <input type="text" class="form-control" name="project_id" value=""
+                        placeholder="Masukkan Project ID">
+                    @error('project_id')
+                        <div class="text-danger">{{ $message }}</div>
+                    @enderror
 
+                    <div class="mb-1 mt-2 label">Nama Project</div>
+                    <input type="text" class="form-control" name="nama_project" value=""
+                        placeholder="Masukkan Nama Project">
+                    @error('nama_project')
+                        <div class="text-danger">{{ $message }}</div>
+                    @enderror
+
+                    <div class="mb-1 mt-2 label">Nama Client</div>
+                    <input type="text" class="form-control" name="nama_client" value=""
+                        placeholder="Masukkan Nama Client">
+                    @error('nama_client')
+                        <div class="text-danger">{{ $message }}</div>
+                    @enderror
+
+                    <div class="mb-1 mt-2 label">Alamat</div>
+                    <input type="text" class="form-control" name="alamat" value=""
+                        placeholder="Masukkan Alamat">
+                    @error('alamat')
+                        <div class="text-danger">{{ $message }}</div>
+                    @enderror
+
+                    <div class="mb-1 mt-2 label">HPP</div>
+                    <input type="text" class="form-control" name="hpp" value=""
+                        placeholder="Masukkan HPP">
+                    @error('hpp')
+                        <div class="text-danger">{{ $message }}</div>
+                    @enderror
+
+                    <div class="mb-1 mt-2 label">RAB</div>
+                    <input type="text" class="form-control" name="rab" value=""
+                        placeholder="Masukkan RAB">
+                    @error('rab')
+                        <div class="text-danger">{{ $message }}</div>
+                    @enderror
+
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">Tambah Data</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</form>
 
     @push('scripts')
         {{ $dataTable->scripts() }}
@@ -105,39 +166,52 @@
                 var tableId = $(this).data('table-id');
                 var name = $(this).data('name');
 
-                if (swal({
-                        text: 'Apa kamu yakin ingin menghapus Project ini ' + name + '?',
-                        buttons: {
-                            cancel: true,
-                            confirm: true,
-                        },
-                    });) {
-                    $.ajax({
-                        url: url,
-                        type: 'DELETE',
-                        data: {
-                            _token: '{{ csrf_token() }}',
-                        },
-                        success: function(result) {
-                            console.log(result); // Cek respons dari server
-                            $('#' + tableId).DataTable().ajax.reload(); // Reload DataTable
-                            swal({
-                                title: 'Berhasil!',
-                                text: 'Project ' + name + ' berhasil dihapus',
-                                icon: 'success',
-                                button: 'OK'
-                            });
-                        },
-                        error: function(xhr) {
-                            swal({
-                                title: 'Gagal!',
-                                text: 'Gagal menghapus project',
-                                icon: 'error',
-                                button: 'OK'
-                            });
+                // Tampilkan SweetAlert konfirmasi
+                swal({
+                    text: 'Apa kamu yakin ingin menghapus Project ini ' + name + '?',
+                    icon: 'warning',
+                    buttons: {
+                        cancel: 'Batal',
+                        confirm: {
+                            text: 'Ya, hapus',
+                            value: true,
+                            visible: true,
+                            className: 'btn btn-danger',
                         }
-                    });
-                }
+                    },
+                    dangerMode: true,
+                }).then((willDelete) => {
+                    if (willDelete) {
+                        // Jika user mengklik "Ya, hapus"
+                        $.ajax({
+                            url: url,
+                            type: 'DELETE',
+                            data: {
+                                _token: '{{ csrf_token() }}',
+                            },
+                            success: function(result) {
+                                $('#' + tableId).DataTable().ajax.reload(); // Reload DataTable
+
+                                // Menampilkan SweetAlert sukses
+                                swal({
+                                    title: 'Berhasil!',
+                                    text: 'Project ' + name + ' berhasil dihapus',
+                                    icon: 'success',
+                                    button: 'OK'
+                                });
+                            },
+                            error: function(xhr) {
+                                // Menampilkan SweetAlert error jika gagal
+                                swal({
+                                    title: 'Gagal!',
+                                    text: 'Gagal menghapus project',
+                                    icon: 'error',
+                                    button: 'OK'
+                                });
+                            }
+                        });
+                    }
+                });
             });
         </script>
     @endpush
