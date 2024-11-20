232fd4e088ceedf187bf4708498a47a3a1851021
fagiln <fagilnuril18@gmail.com>
2024-11-20 10:09:59 +0700

    [add] Vendor CruD

---
+++ app/Http/Controllers/Master/DataVendorController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers\Master;
+
+use App\DataTables\Master\DataVendorDataTable;
+use App\Http\Controllers\Controller;
+use App\Models\DataVendor;
+use Illuminate\Http\Request;
+
+class DataVendorController extends Controller
+{
+    public function index(DataVendorDataTable $dataTable)
+    {
+        return $dataTable->render('master.data-vendor.index');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nama_vendor' => 'required|string|max:50',
+            'alamat_vendor' => 'required|string',
+            'kota' => 'required|string|max:40',
+            'no_tlp' => 'required|numeric',
+            'email' => 'required|email',
+            'up' => 'required|string',
+        ], [
+            'nama_vendor.required' => 'Nama vendor wajib diisi.',
+            'nama_vendor.string' => 'Nama vendor harus berupa teks.',
+            'nama_vendor.max' => 'Nama vendor tidak boleh lebih dari 50 karakter.',
+            'alamat_vendor.required' => 'Alamat vendor wajib diisi.',
+            'alamat_vendor.string' => 'Alamat vendor harus berupa teks.',
+            'kota.required' => 'Kota wajib diisi.',
+            'kota.string' => 'Kota harus berupa teks.',
+            'kota.max' => 'Nama kota tidak boleh lebih dari 40 karakter.',
+            'no_tlp.required' => 'Nomor telepon wajib diisi.',
+            'no_tlp.numeric' => 'Nomor telepon harus berupa angka.',
+            'email.required' => 'Email wajib diisi.',
+            'email.email' => 'Format email tidak valid.',
+            'up.required' => 'Nama UP wajib diisi.',
+            'up.string' => 'Nama UP harus berupa teks.',
+        ]);
+
+        $data = [
+            'nama_vendor'  => $request->nama_vendor,
+            'alamat_vendor'  => $request->alamat_vendor,
+            'kota'  => $request->kota,
+            'no_tlp'  => $request->no_tlp,
+            'email'  => $request->email,
+            'up'  => $request->up,
+        ];
+        DataVendor::create($data);
+        return response()->json(['success' => 'Data berhasil di tambah!'], 200);
+    }
+
+    public function edit(string $id)
+    {
+        $vendor= DataVendor::find($id);
+        // dd($projectid);
+        return response()->json($vendor);
+    }
+    public function update(Request $request, string $id)
+    {
+        $vendor = DataVendor::findOrFail($id); // tambahkan fail jika id tidak ditemukan
+        $request->validate([
+            'nama_vendor_edit' => 'required|string|max:50',
+            'alamat_vendor_edit' => 'required|string',
+            'kota_edit' => 'required|string|max:40',
+            'no_tlp_edit' => 'required|numeric',
+            'email_edit' => 'required|email',
+            'up_edit' => 'required|string',
+        ], [
+           'nama_vendor_edit.required' => 'Nama vendor wajib diisi.',
+            'nama_vendor_edit.string' => 'Nama vendor harus berupa teks.',
+            'nama_vendor_edit.max' => 'Nama vendor tidak boleh lebih dari 50 karakter.',
+            'alamat_vendor_edit.required' => 'Alamat vendor wajib diisi.',
+            'alamat_vendor_edit.string' => 'Alamat vendor harus berupa teks.',
+            'kota_edit.required' => 'Kota wajib diisi.',
+            'kota_edit.string' => 'Kota harus berupa teks.',
+            'kota_edit.max' => 'Nama kota tidak boleh lebih dari 40 karakter.',
+            'no_tlp_edit.required' => 'Nomor telepon wajib diisi.',
+            'no_tlp_edit.numeric' => 'Nomor telepon harus berupa angka.',
+            'email_edit.required' => 'Email wajib diisi.',
+            'email_edit.email' => 'Format email tidak valid.',
+            'up_edit.required' => 'Nama UP wajib diisi.',
+            'up_edit.string' => 'Nama UP harus berupa teks.',
+        ]);
+
+
+        // Update data dengan input yang sesuai
+        $vendor->update([
+            'nama_vendor'  => $request->nama_vendor_edit,
+            'alamat_vendor'  => $request->alamat_vendor_edit,
+            'kota'  => $request->kota_edit,
+            'no_tlp'  => $request->no_tlp_edit,
+            'email'  => $request->email_edit,
+            'up'  => $request->up_edit,
+        ]);
+
+        return response()->json([
+            'success' => 'Data Vendor berhasil di edit!'
+        ], 200);
+
+        // return redirect()->back();
+    }
+
+
+    public function delete(string $id)
+    {
+        $vendor = DataVendor::findOrFail($id);
+        $vendor->delete();
+        return response()->json([
+            'success' => 'Data vendor berhasil dihapus!'
+        ], 200);
+    }
+}
