5ec7a6fef6b3c0b83c6620cb840b04dabc11e4cb
fagiln <fagilnuril18@gmail.com>
2024-11-25 10:07:42 +0700

    [fix] index po

--- resources/views/po/index.blade.php
+++ resources/views/po/index.blade.php
@@ -33,7 +33,111 @@
     </div>
 
     <!-- Form Tambah -->
--
+    <div id="form-section" style="display:none;" class="m-4 ">
+
+        <form action="{{ route('po.store') }}" method="POST" id="formTambah">
+            @csrf
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="card p-4-">
+                        <h5 class="card-title">Detail</h5>
+                        <div class="mb-1 mt-4 label">Nama Barang</div>
+                        <input type="text" class="form-control" name="nama_vendor" id="nama_vendor" value=""
+                            placeholder="Masukkan Nama Vendor">
+                        @error('nama_vendor')
+                            <div class="text-danger error">{{ $message }}</div>
+                        @enderror
+
+                        <div class="mb-1 mt-2 label">Qty</div>
+                        <input type="text" class="form-control" name="nama_vendor" id="nama_vendor" value=""
+                            placeholder="Masukkan Nama Vendor">
+                        @error('nama_vendor')
+                            <div class="text-danger error">{{ $message }}</div>
+                        @enderror
+
+                        <div class="mb-1 mt-2 label">Satuan</div>
+                        <input type="text" class="form-control" name="nama_vendor" id="nama_vendor" value=""
+                            placeholder="Masukkan Nama Vendor">
+                        @error('nama_vendor')
+                            <div class="text-danger error">{{ $message }}</div>
+                        @enderror
+
+                        <div class="mb-1 mt-2 label">Harga Satuan</div>
+                        <input type="text" class="form-control" name="nama_vendor" id="nama_vendor" value=""
+                            placeholder="Masukkan Nama Vendor">
+                        @error('nama_vendor')
+                            <div class="text-danger error">{{ $message }}</div>
+                        @enderror
+                        <div class="d-flex justify-content-end mt-3">
+
+                            <button type="submit" class="btn btn-primary">Add</button>
+                        </div>
+
+
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="card p-4">
+                        <h5 class="card-title">Header</h5>
+                        <div class="mb-1 mt-4 label">Kode Purchase Order</div>
+                        <input type="text" class="form-control" name="kota" id="kota" value=""
+                            placeholder="Masukkan Kota">
+                        @error('kota')
+                            <div class="text-danger error">{{ $message }}</div>
+                        @enderror
+
+                        <div class="mb-1 mt-2 label">Tanggal</div>
+                        <input type="text" class="form-control" name="no_tlp" id="no_tlp" value=""
+                            placeholder="Masukkan No Telpon">
+                        @error('no_tlp')
+                            <div class="text-danger error">{{ $message }}</div>
+                        @enderror
+
+                        <div class="mb-1 mt-2 label">Nama Vendor</div>
+                        <input type="text" class="form-control" name="email" id="email" value=""
+                            placeholder="Masukkan Email">
+                        @error('email')
+                            <div class="text-danger error">{{ $message }}</div>
+                        @enderror
+
+                        <div class="mb-1 mt-2 label">Nama Buyer </div>
+                        <input type="text" class="form-control" name="up" id="up" value=""
+                            placeholder="Masukkan up">
+                        @error('up')
+                            <div class="text-danger error">{{ $message }}</div>
+                        @enderror
+                        <div class="mb-1 mt-2 label">Catatan</div>
+                        <input type="text" class="form-control" name="up" id="up" value=""
+                            placeholder="Masukkan up">
+                        @error('up')
+                            <div class="text-danger error">{{ $message }}</div>
+                        @enderror
+                        <div class="mb-1 mt-2 label">Catatan 2 </div>
+                        <input type="text" class="form-control" name="up" id="up" value=""
+                            placeholder="Masukkan up">
+                        @error('up')
+                            <div class="text-danger error">{{ $message }}</div>
+                        @enderror
+
+                        <div class="mb-1 mt-2 label">Diskon Nominal Rupiah </div>
+                        <input type="text" class="form-control" name="up" id="up" value=""
+                            placeholder="Masukkan up">
+                        @error('up')
+                            <div class="text-danger error">{{ $message }}</div>
+                        @enderror
+                        <div class="d-flex justify-content-end mt-3">
+                            <button type="submit" class="btn btn-primary">Tambah Data</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class=" d-md-flex justify-content-end">
+                <button type="button" class="btn btn-secondary me-2" id="btn-cancel">Close</button>
+            </div>
+        </form>
+   
+    </div>
+
 
     <!-- Modal Edit -->
 
@@ -45,7 +149,7 @@
                     swal('Berhasil!', '{{ session('success') }}', 'success');
                 @endif
             });
--
+
             $(document).on('click', 'button[data-action="delete"]', function() {
                 var url = $(this).data('url');
                 var tableId = $(this).data('table-id');
