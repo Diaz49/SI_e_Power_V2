e1e9ad89b3e4564042b72231d72098c4acf24486
Diaz Bagus Maulana <94949041+Diaz49@users.noreply.github.com>
2024-12-13 09:29:27 +0700

    update bast

--- resources/views/bast/index.blade.php
+++ resources/views/bast/index.blade.php
@@ -42,27 +42,51 @@
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
-                        <div class="mb-1 label">Nama Bank</div>
-                        <input type="text" class="form-control" name="nama_bank" value="{{ old('') }}"
-                            placeholder="Masukkan Nama Bank">
-                        @error('nama_bank')
-                            <div class="text-danger">{{ $message }}</div>
-                        @enderror
+                        <div>
+                            <div class="mb-1 label">Tanggal BAST</div>
+                            <input type="date" class="form-control" name="nama_bank" value="{{ old('') }}"
+                                placeholder="Masukkan Nama Bank">
+                            {{-- @error('nama_bank')
+                                <div class="text-danger">{{ $message }}</div>
+                            @enderror --}}
 
-                        <div class="mb-1 mt-2 label">Nama Rek.</div>
-                        <input type="text" class="form-control" name="nama_rek" value="{{ old('') }}"
-                            placeholder="Masukkan Nama Rekening">
-                        @error('nama_rek')
-                            <div class="text-danger">{{ $message }}</div>
-                        @enderror
-
-                        <div class="mb-1 mt-2 label">Nomer Rek.</div>
+                            <div class="mb-1 mt-2 label">Nomer Invoice</div>
+                            <input type="text" class="form-control" name="nama_rek" value="{{ old('') }}"
+                                placeholder="Masukkan Nama Rekening">
+                            {{-- @error('nama_rek')
+                                <div class="text-danger">{{ $message }}</div>
+                            @enderror --}}
+                        </div>
+                        <div class="mb-1 mt-2 label">Nama</div>
                         <input type="text" class="form-control" name="nomer_rek" value="{{ old('') }}"
                             placeholder="Masukkan Nomer Rekening">
-                        @error('nomer_rek')
+                        {{-- @error('nomer_rek')
                             <div class="text-danger">{{ $message }}</div>
-                        @enderror
-
+                        @enderror --}}
+                        <div class="mb-1 mt-2 label">Jabatan</div>
+                        <input type="text" class="form-control" name="nomer_rek" value="{{ old('') }}"
+                            placeholder="Masukkan Nomer Rekening">
+                        {{-- @error('nomer_rek')
+                            <div class="text-danger">{{ $message }}</div>
+                        @enderror --}}
+                        <div class="mb-1 mt-2 label">Deskripsi Singkat</div>
+                        <input type="text" class="form-control" name="nomer_rek" value="{{ old('') }}"
+                            placeholder="Masukkan Nomer Rekening">
+                        {{-- @error('nomer_rek')
+                            <div class="text-danger">{{ $message }}</div>
+                        @enderror --}}
+                        <div class="mb-1 mt-2 label">Jumlah Item</div>
+                        <input type="text" class="form-control" name="nomer_rek" value="{{ old('') }}"
+                            placeholder="Masukkan Nomer Rekening">
+                        {{-- @error('nomer_rek')
+                            <div class="text-danger">{{ $message }}</div>
+                        @enderror --}}
+                        <div class="mb-1 mt-2 label">Total Invoice</div>
+                        <input type="text" class="form-control" name="nomer_rek" value="{{ old('') }}"
+                            placeholder="Masukkan Nomer Rekening">
+                        {{-- @error('nomer_rek')
+                            <div class="text-danger">{{ $message }}</div>
+                        @enderror --}}
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
@@ -72,4 +96,71 @@
             </div>
         </div>
     </form>
+    
+    <!-- Modal Tambah -->
+    {{-- <form action="{{ route('bast.edit') }}" method="PUT"> --}}
+        @csrf
+        <div class="modal fade" id="modalTambah" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">Tambah Data Bank</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <div>
+                            <div class="mb-1 label">Tanggal BAST</div>
+                            <input type="date" class="form-control" name="nama_bank" value="{{ old('') }}"
+                                placeholder="Masukkan Nama Bank">
+                            {{-- @error('nama_bank')
+                                <div class="text-danger">{{ $message }}</div>
+                            @enderror --}}
+
+                            <div class="mb-1 mt-2 label">Nomer Invoice</div>
+                            <input type="text" class="form-control" name="nama_rek" value="{{ old('') }}"
+                                placeholder="Masukkan Nama Rekening">
+                            {{-- @error('nama_rek')
+                                <div class="text-danger">{{ $message }}</div>
+                            @enderror --}}
+                        </div>
+                        <div class="mb-1 mt-2 label">Nama</div>
+                        <input type="text" class="form-control" name="nomer_rek" value="{{ old('') }}"
+                            placeholder="Masukkan Nomer Rekening">
+                        {{-- @error('nomer_rek')
+                            <div class="text-danger">{{ $message }}</div>
+                        @enderror --}}
+                        <div class="mb-1 mt-2 label">Jabatan</div>
+                        <input type="text" class="form-control" name="nomer_rek" value="{{ old('') }}"
+                            placeholder="Masukkan Nomer Rekening">
+                        {{-- @error('nomer_rek')
+                            <div class="text-danger">{{ $message }}</div>
+                        @enderror --}}
+                        <div class="mb-1 mt-2 label">Deskripsi Singkat</div>
+                        <input type="text" class="form-control" name="nomer_rek" value="{{ old('') }}"
+                            placeholder="Masukkan Nomer Rekening">
+                        {{-- @error('nomer_rek')
+                            <div class="text-danger">{{ $message }}</div>
+                        @enderror --}}
+                        <div class="mb-1 mt-2 label">Jumlah Item</div>
+                        <input type="text" class="form-control" name="nomer_rek" value="{{ old('') }}"
+                            placeholder="Masukkan Nomer Rekening">
+                        {{-- @error('nomer_rek')
+                            <div class="text-danger">{{ $message }}</div>
+                        @enderror --}}
+                        <div class="mb-1 mt-2 label">Total Invoice</div>
+                        <input type="text" class="form-control" name="nomer_rek" value="{{ old('') }}"
+                            placeholder="Masukkan Nomer Rekening">
+                        {{-- @error('nomer_rek')
+                            <div class="text-danger">{{ $message }}</div>
+                        @enderror --}}
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary">Edit Data</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
+
 @endsection
